51a8b2e25519f4064bea7851425c7d4702178d2d
unknown <pablocast253@gmail.com>
2024-11-07 19:46:34 -0800

    random button work

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,6 +1,34 @@
 // TODO java script structure 
 
+
 //TODO create variables with a selection query that selects elements from the HTML doc
+
+let randomButton = document.querySelector("#pickForMe");
+ 
+ let randomCoffeeType =  ["Espresso", "Latte", "Cappuccino", "Mocha", "Americano"];
+function pickRandomCoffee (randomCoffeeType) {
+    // Get a random index
+const randomIndex = Math.floor(Math.random() * randomCoffeeType.length);
+
+// Use the random index to access an item in the array
+let randomCoffee = randomCoffeeType[randomIndex];
+
+console.log(randomCoffee);
+
+return randomCoffee;
+ };
+ randomButton.addEventListener("click", () => pickRandomCoffee(randomCoffeeType));
+//  addEventListener('click', pickRandomCoffee);
+
+const coffee = {
+
+    temp:  undefined,
+    milk: undefined,
+    type: undefined ,
+    flavors: undefined
+
+}
+
 
 // TODO create other varibles possably needed for the code
 
@@ -9,4 +37,4 @@
 
 // TODO create method 
 
-// TODOcreate event listener for button presses and call functions to run when event happens 
+// TODOcreate event listener for button presses and call functions to run when event 
